a0e0a083f9966783e6b357ec6c9f21e58a22e84c
kanhaiyapanchal <kanhaiyapanchal03@gmail.com>
2023-04-12 18:55:45 +0530

    add aportl

---
+++ assginment-portal/public_html/sub_files/phpdemo.php
@@ -0,0 +1,86 @@
+<?php include('../header.include.php'); ?>
+<div class="container mt-5  main-container" >
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+           
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        <div class="col-12">
+            <h2 class="text-primary">DUmmy Assignemnt</h2>
+            Haaree Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nobis vel doloribus doloremque
+            amet esse porro ullam ipsa voluptas totam, officiis delectus iure, ut voluptates quibusdam distinctio
+            similique! Quae, tempora dicta quam iure voluptatem reiciendis possimus libero, ipsa, eaque dolorem quod
+            nobis? Est facere perferendis asperiores sapiente velit quas illo reprehenderit consectetur temporibus sequi
+            repudiandae accusantium ut, nam animi. Corrupti laudantium consequuntur reiciendis explicabo ea officiis
+            quod, facere quae magni accusamus dolorum cumque dolores minima tempora iste? Molestiae corporis iusto
+            excepturi corrupti quos nobis hic, nesciunt enim adipisci ex! Repellat ex corrupti id aperiam similique
+            quisquam consequatur nulla voluptate odio.
+        </div>
+        
+    </div>
+</div>
+
+<?php include('../footer.inlude.php'); ?>
